bd6b1736e2867f343bc96c776b80855ddbb856d3
HamudyAL <145443368+HamudyAL@users.noreply.github.com>
2023-09-20 14:55:27 +0200

    Update streamlit_app.py
    URLError

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,34 @@
 
-import streamlit
+#import streamlit
 streamlit.title('🥣 My Parents new healthy Diner')
 streamlit.header('🥗 Breakfast Menu')
 streamlit.text('🐔 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥑 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🍞Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-import pandas
+
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
-import snowflake.connector
+streamlit.stop()
+
+#import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
@@ -35,4 +36,8 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
+
+
+
+from urllib.error import URLError 
 streamlit.write('The user entered ', fruit_choice)
