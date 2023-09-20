e72c461e81c7433427b5651aeef7b3eeaacd4931
HamudyAL <145443368+HamudyAL@users.noreply.github.com>
2023-09-20 15:33:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,15 @@
 import streamlit
+import requests
+import pandas
+import snowflake.connector
+from urllib.error import URLError 
 streamlit.title('🥣 My Parents new healthy Diner')
 streamlit.header('🥗 Breakfast Menu')
 streamlit.text('🐔 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥑 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🍞Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-import requests
-import pandas
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
@@ -33,7 +36,7 @@ streamlit.error()
 
 streamlit.stop()
 
-import snowflake.connector
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
@@ -43,4 +46,4 @@ streamlit.dataframe(my_data_rows)
 fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
 
 streamlit.write('The user entered ', fruit_choice)
-from urllib.error import URLError 
+
