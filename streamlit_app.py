646d8ab392466341ae558a2914ba664875c53831
HamudyAL <145443368+HamudyAL@users.noreply.github.com>
2023-09-20 15:36:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,4 @@
 import streamlit
-import requests
-import pandas
-import snowflake.connector
-from urllib.error import URLError 
 streamlit.title('🥣 My Parents new healthy Diner')
 streamlit.header('🥗 Breakfast Menu')
 streamlit.text('🐔 Omega 3 & Blueberry Oatmeal')
@@ -10,6 +6,7 @@ streamlit.text('🥑 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🍞Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
@@ -18,25 +15,19 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
-
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-try: 
- 
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice: 
-    stream.error("Please select a fruit to get information.")
-  else:
-    
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
+
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
-except URLError as e: 
-streamlit.error()
-
 streamlit.stop()
 
-
+import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
@@ -46,4 +37,4 @@ streamlit.dataframe(my_data_rows)
 fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
 
 streamlit.write('The user entered ', fruit_choice)
-
+from urllib.error import URLError 
